6b1d1f40621d5fbf8952fefea614dc8c25c78dbe
Alexander John <96aljohn@gmail.com>
2024-09-08 10:52:18 -0700

    rewrite-main-with-onclick

--- scripts/main.js
+++ scripts/main.js
@@ -4,9 +4,7 @@ function random(num)  {
     return Math.floor(Math.random() * (num + 1));
 }
 
-function changeBackground() {
+btn.onClick = () => {
     const rndColor = `rgb(${random(255)} ${random(255)} ${random(255)})`;
     document.body.style.backgroundColor = rndColor;
 }
-
-btn.addEventListener("click", changeBackground);
